051cd56417de83a9c26d4bf981d4013770855f0f
Cooper Wutzke <cooperwutzke97@gmail.com>
2024-02-12 20:08:51 -0800

    Working on game.py
    Adding agent creation and agent login. Working on display available factions at creation. Current issue is parsing all the faction names out of a JSON cleanly with only names and description remaining.

--- faction.py
+++ faction.py
@@ -3,10 +3,22 @@
 
 import requests
 import json
+import time
 
 def get_factions():
     factions = requests.get('https://api.spacetraders.io/v2/factions')
-    factions_json = factions.json()
-    factions_str = json.dumps(factions_json, indent=4)
-    print(factions_str)
-    return factions_json
+    factions_response = factions.json()
+    print(type(factions_response))
+    time.sleep(4)
+    print(json.dumps(factions_response, indent=4))
+    time.sleep(4)
+    return factions_response
+
+def filter_factions(tag):
+    faction_list = get_factions()
+    names_list = []
+    print(faction_list)
+    for fac in faction_list:
+        if (fac[tag]):
+            names_list.append(fac)
+    return names_list
--- game.py
+++ game.py
@@ -2,27 +2,70 @@
 # Game Start Logic
 # https://docs.spacetraders.io/
 
+# MOCK SERVER - This way we don't register agents till we are ready to play
+# https://stoplight.io/mocks/spacetraders/spacetraders/96627693/register
+ 
+
 import logging
 import requests
 import json
+import faction
 
-#logging.basicConfig(filename='history.log', encoding='utf-8', format='%(asctime)s | %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+logging.basicConfig(filename='history.log', encoding='utf-8', format='%(asctime)s | %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+current_player = {}
 
-def newagent(callsign, faction):
-    newagent_request = requests.post(url='https://api.spacetraders.io/v2/register',
+#View some global element such as announcements, server reset dates, player count, and leaderboards
+def get_gameinfo():
+    gameinfo_request = requests.get(url='https://stoplight.io/mocks/spacetraders/spacetraders/96627693/')
+    gameinfo_response = gameinfo_request.json()
+    return gameinfo_response
+
+#Make sure servers are online
+def get_status():
+    status_json = get_gameinfo()['status']
+    return(json.dumps(status_json, indent=4))
+
+def get_announcements():
+    announcement_json = get_gameinfo()['announcements']
+    return(json.dumps(announcement_json, indent=4))
+
+def get_agentcount():
+    agentcount_json = get_gameinfo()['stats']['agents']
+    return(json.dumps(agentcount_json, indent=4))
+
+
+def post_newagent(callsign, faction):
+    newagent_request = requests.post(url='https://stoplight.io/mocks/spacetraders/spacetraders/96627693/register',
                                      headers='Content-Type: application/json',
                                      json={'symbol': callsign, 'faction': faction})
     newagent_response = newagent_request.json()
-    print(newagent_response)
+    print(json.dumps(newagent_response))
 
-    with open('agent_data.json', 'w') as file:
-        json.dump(newagent_response, file)
+    with open('{callsign}_data.json', 'w') as file:
+        json.dumps(newagent_response, file, indent=4)
 
-    logging.debug('Agent File Created: {file}')
-    print("Agent File Created: {file}")
+    logging.info('Agent File Created: {file}')
 
-def gamelogin(access_token):
-    logging.debug('Successful Login: %s', access_token)
+# Logging in is simply selecting which access token all of our functions will target
+def get_myagent(access_token):
+    myagent_request = requests.get(url='https://stoplight.io/mocks/spacetraders/spacetraders/96627693/my/agent',
+                                   headers='Authorization: {access_token}')
+    try:
+        myagent_json = json.loads(myagent_request)
+    except ValueError as e:
+        logging.debug('Invalid login token: %s', access_token)
+    logging.info('Successful Login: %s', access_token)
     print('Successful Login: %s', access_token)
+    return myagent_json
 
-        
+def test_game():
+    print('Enter new agent callsign: ')
+    callsign_str = str(input())
+    print(callsign_str)
+    print('Choose a faction: ')
+    selected_factions = faction.filter_factions('name')
+    print(selected_factions)
+    faction_str = str(input())
+    print('%s', faction_str)
+
+test_game()
